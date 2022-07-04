583bfd022ae4e2b2ace9daeea24e178f515074a3
Sioun <240061681@qq.com>
2022-07-04 16:34:51 +0100

    first add

---
+++ npztest.py
@@ -0,0 +1,38 @@
+from numpy import load
+import cv2
+
+data = load('E:\\2021-2022 Msc\pytorch-superpoint-master\pytorch-superpoint-master\logs\SuperPoint_COCO_to_Synthetic_test\predictions\\'
+            'train\Frames_S1\FrameBuffer_0008.npz')
+data2 = load('logs\superpoint_test90000\predictions\\train\Frames_S1\FrameBuffer_0008.npz')
+pic = cv2.imread('E:\\2021-2022 Msc\dataset\SyntheticColon_I\Frames_S1\FrameBuffer_0008.png')
+lst = data.files
+lst2 = data2.files
+for item in lst:
+    pst = data[item]
+    print(data[item])
+# for item in lst2:
+    pst2 = data2[item]
+    # print(data2[item])
+pic2 = cv2.resize(pic,(472, 472))
+pic = cv2.resize(pic,(472, 472))
+r,r2 = 0,0
+
+print(pst.shape)
+print(pst2.shape)
+for psti in pst:
+    if psti[2] >= 0.03:
+        cv2.circle(pic, [psti[0].astype(int),psti[1].astype(int)], 1, (0,225,0))
+    else:
+        cv2.circle(pic, [psti[0].astype(int), psti[1].astype(int)], 1, (0, 0, 225))
+        r += 1
+for psti in pst2:
+    if psti[2] > 0.03:
+        cv2.circle(pic2, [psti[0].astype(int),psti[1].astype(int)], 1, (0,225,0))
+
+    else:
+        cv2.circle(pic2, [psti[0].astype(int), psti[1].astype(int)], 1, (0, 0, 225))
+        r2+=1
+print(r,r2)
+cv2.imshow('img',pic)
+cv2.imshow('img2',pic2)
+cv2.waitKey(0)
---
+++ settings.py
@@ -0,0 +1,10 @@
+"""
+paths defined here are used in many places
+"""
+
+DATA_PATH = 'E:\\2021-2022 Msc\dataset\SyntheticColon_I' # path for datasets
+EXPER_PATH = 'logs' # path for saving checkpoints
+SYN_TMPDIR = 'E:\\2021-2022 Msc\dataset\SyntheticColon_I'  # path for dumping synthetic data
+DEBUG = False # true: will make synthetic data only uses draw_checkboard and ignore other classes
+# DEBUG = False
+
